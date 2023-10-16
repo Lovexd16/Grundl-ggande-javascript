a54ad0899cc580175605cc8b2fff2bfdf9866be9
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-10-16 15:24:24 +0200

    bytt variablerna

--- Övning hello world med javascript/script.js
+++ Övning hello world med javascript/script.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() { //Laddar DOM innan manipulation
-    var h1Element = document.createElement("h1"); //Skapar h1 element
+    var titleMyName = document.createElement("h1"); //Skapar h1 element
 
-    var p1Element = document.createElement("p1"); //Skapar p1 element
+    var description = document.createElement("p1"); //Skapar p1 element
 
-    var items = ["Laga mat", "Köra bil", "League"] //Skapar items till lista
-    var ul = document.createElement("ul"); //Skapar ul element
+    var listItems = ["Laga mat", "Köra bil", "League"] //Skapar items till lista
+    var list = document.createElement("ul"); //Skapar ul element
 
-    for (var i = 0; i < items.length; i++) { //For loop för att loopa listan 
+    for (var i = 0; i < listItems.length; i++) { //For loop för att loopa listan 
         var li = document.createElement("li"); //Skapar li element
-        li.textContent = items[i];
-        ul.appendChild(li);
+        li.textContent = listItems[i];
+        list.appendChild(li);
     }
 
 
-    h1Element.textContent = "Love Rumar Karlquist"; //Text innehåll av h1
+    titleMyName.textContent = "Love Rumar Karlquist"; //Text innehåll av h1
 
-    p1Element.textContent = "Hej! Jag är 21 år gammal och pluggar till Javautvecklare. På fritiden spenderar jag mycket tid på gymmet, lagar mat och umgås med familjen. AIK är en stor passion i mitt liv, och jag kollar alltid när de spelar!";
+    description.textContent = "Hej! Jag är 21 år gammal och pluggar till Javautvecklare. På fritiden spenderar jag mycket tid på gymmet, lagar mat och umgås med familjen. AIK är en stor passion i mitt liv, och jag kollar alltid när de spelar!";
 
-    document.body.appendChild(h1Element);
+    document.body.appendChild(titleMyName);
 
-    document.body.appendChild(p1Element);
+    document.body.appendChild(description);
 
-    document.body.appendChild(ul); //Lägger till elementet till body
+    document.body.appendChild(list); //Lägger till elementet till body
 });
 
 
