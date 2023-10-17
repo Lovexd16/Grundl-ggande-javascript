34b017eb5cb08d6b107c0a84cd3635a1f97e4fb2
Love Rumar Karlquist <lovebandy@hotmail.se>
2023-10-17 10:29:58 +0200

    if och switch med javascript

---
+++ If och switch med javascript/script.js
@@ -0,0 +1,32 @@
+// const names = "Pelle"; //Ändra namn här för att testa
+
+// if (names == "Lars") {
+//     console.log("Ja han heter lars!");
+// } else if (names == "Pelle") {
+//     console.log("Yep, här är Pelle!");
+// } else {
+//     console.log("Hittar ingen!");
+// }
+
+const names = "Anna"; //Ändra namn här för att testa!
+
+switch(names) {
+    case "Kalle":
+        console.log("Japp, det är Kalle");
+    break;
+
+    case "Anna":
+        console.log("Anna är här!");
+    break;
+
+    case "Bengt":
+        console.log("Bengt är hemma!");
+    break;
+
+    case "Oskar":
+        console.log("Oskar är inne!");
+    break;
+
+    default:
+        console.log("Inget sådant namn här!");
+}
